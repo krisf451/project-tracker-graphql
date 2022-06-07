804b11b3f0617c583592a2aaaa39f73d75ee2526
Kristian Fulkerson <kristianf451@gmail.com>
2022-06-07 16:01:59 -0700

    feat: frontend init :alien: going to use bootstrap instead of tailwind this time

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function App() {
   return (
-    <div className="min-h-screen w-full flex justify-center items-center">
-      <h1 className="text-green-500 text-3xl underline">Test Tailwind</h1>
+    <div className="container">
+      <h1>Test Tailwind</h1>
     </div>
   );
 }
