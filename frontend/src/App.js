7d46ea8e0dcb1d8752f14fcd6da1b66653e3fffc
Kristian Fulkerson <kristianf451@gmail.com>
2022-06-07 12:49:45 -0700

    feat: frontend init :alien:

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen w-full flex justify-center items-center">
+      <h1 className="text-green-500 text-3xl underline">Test Tailwind</h1>
+    </div>
+  );
+}
+
+export default App;
